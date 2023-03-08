0ba5e80dd4e06eee9cee42128116ed8da0d826c0
askar <asinible@gmail.com>
2023-03-08 23:28:23 +0600

    app structure added

--- project/app/main.py
+++ project/app/main.py
@@ -1,25 +1,49 @@
 # project/app/main.py
-import os
+
+import logging
 
 from fastapi import FastAPI, Depends
 from tortoise.contrib.fastapi import register_tortoise
 
 from app.config import get_settings, Settings
 
-app = FastAPI()
+from app.api import ping
+from app.db import init_db
 
-register_tortoise(
-    app,
-    db_url=os.environ.get("DATABASE_URL"),
-    modules={"models": ["app.models.tortoise"]},
-    generate_schemas=False,
-    add_exception_handlers=True,
-)
+log = logging.getLogger("uvicorn")
 
 
-@app.get("/ping")
-async def pong(settings: Settings = Depends(get_settings)):
-    return {"ping": "pongster!",
-            "environment": settings.environment,
-            "testing": settings.testing
-            }
+def create_application() -> FastAPI:
+    application = FastAPI()
+
+    register_tortoise(
+        application,
+        db_url=os.environ.get("DATABASE_URL"),
+        modules={"models": ["app.models.tortoise"]},
+        generate_schemas=False,
+        add_exception_handlers=True,
+    )
+    application.include_router(ping.router)
+
+    return application
+
+
+app = create_application()
+
+
+@app.on_event("startup")
+async def startup_event():
+    log.info('Starting up ...')
+    init_db(app)
+
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    log.info("Shutting down ...")
+
+# @app.get("/ping")
+# async def pong(settings: Settings = Depends(get_settings)):
+#     return {"ping": "pongster!",
+#             "environment": settings.environment,
+#             "testing": settings.testing
+#             }
