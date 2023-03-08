a756526276f29e7152df46f057c2eadb122aa029
askar <asinible@gmail.com>
2023-03-08 23:59:48 +0600

    add rest routes

--- project/app/main.py
+++ project/app/main.py
@@ -7,23 +7,30 @@ from tortoise.contrib.fastapi import register_tortoise
 
 from app.config import get_settings, Settings
 
-from app.api import ping
+from app.api import ping, summaries
 from app.db import init_db
 
 log = logging.getLogger("uvicorn")
 
 
+# def create_application() -> FastAPI:
+#     application = FastAPI()
+#
+#     register_tortoise(
+#         application,
+#         db_url=os.environ.get("DATABASE_URL"),
+#         modules={"models": ["app.models.tortoise"]},
+#         generate_schemas=False,
+#         add_exception_handlers=True,
+#     )
+#     application.include_router(ping.router)
+#
+#     return application
+
 def create_application() -> FastAPI:
     application = FastAPI()
-
-    register_tortoise(
-        application,
-        db_url=os.environ.get("DATABASE_URL"),
-        modules={"models": ["app.models.tortoise"]},
-        generate_schemas=False,
-        add_exception_handlers=True,
-    )
     application.include_router(ping.router)
+    application.include_router(summaries.router, prefix="/summaries", tags=["summaries"])  # new
 
     return application
 
