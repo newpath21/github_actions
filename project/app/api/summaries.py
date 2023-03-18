9d08e79a7342dc17e3bd7827fefa9426a06c2c8e
askar <asinible@gmail.com>
2023-03-19 01:43:42 +0600

    add background_tasks

--- project/app/api/summaries.py
+++ project/app/api/summaries.py
@@ -2,7 +2,7 @@
 import json
 from typing import List
 
-from fastapi import APIRouter, HTTPException, Path
+from fastapi import APIRouter, HTTPException, Path, BackgroundTasks
 
 from app.api import crud
 from app.models.pydantic import (  # isort:skip
@@ -11,13 +11,16 @@ from app.models.pydantic import (  # isort:skip
     SummaryUpdatePayloadSchema,
 )
 from app.models.tortoise import SummarySchema
+from app.summarizer import generate_summary
 
 router = APIRouter()
 
 
 @router.post("/", response_model=SummaryResponseSchema, status_code=201)
-async def create_summary(payload: SummaryPayloadSchema) -> SummaryResponseSchema:
+async def create_summary(payload: SummaryPayloadSchema, background_tasks: BackgroundTasks) -> SummaryResponseSchema:
     summary_id = await crud.post(payload)
+
+    background_tasks.add_task(generate_summary, summary_id, payload.url)
 
     response_object = {
         "id": summary_id,
